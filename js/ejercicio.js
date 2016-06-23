ef9c2dc8ec17c2647523409f33d69584f0920c62
natuchasca <natuxalee@gmail.com>
2016-06-23 12:19:18 -0400

    Trabajando objetos js

---
+++ js/ejercicio.js
@@ -0,0 +1,75 @@
+// EJERCICIOS OBJETOS
+// Quitar comentarios para visualizar uno a uno
+
+
+
+//#1 IFORMACION FACTURA
+var factura = {
+  empresa : {
+    nombre : "Nombre de Empresa",
+    direccion : "Direccion de Empresa",
+    telefono : "000 000 000",
+    NIF : "", 
+  },
+  cliente : {
+    nombre : "Nombre de Cliente",
+    direccion : "Direccion de Cliente",
+    telefono : "111 111 111",
+    NIF : "", 
+  },
+  elementos : [
+  {descripcion: "Producto", precio: 0, cantidad: 0}
+  {descripcion: "Producto", precio: 0, cantidad: 0}
+  {descripcion: "Producto", precio: 0, cantidad: 0}
+  ],
+  pago : {
+    total : "0",
+    iva : "0",
+    formaDePago : "0",
+  }
+};
+
+
+    
+
+
+/*
+//#2 CUATRIMESTRE ACTUAL  /    OK!
+var fechaActual = new Date();
+
+if (fechaActual.getMonth() >= 0 && fechaActual.getMonth() < 4) {
+  console.log("Estamos en 1er tremestre");
+}
+else if (fechaActual.getMonth() >= 4 && fechaActual.getMonth() < 8) {
+ console.log("Estamos en el 2do tremestre");
+}
+else {
+  console.log("Estamos en 3er tremestre");
+}
+*/
+
+
+
+
+//#3 VALOR FUTURO DE UNA INVERSION
+
+
+
+
+//#4 OBJETO CIRCULO
+
+
+
+
+//#5 IDENTIFICAR LLAMADA
+
+
+
+//#6 INDICE DE MASA CORPORAL
+
+
+
+//#7 GASOLINERA
+
+
+
